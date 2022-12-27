cde74b59d4ec950fd0a4fdbb874fbdc9473d3bd0
Andrii Chubatiuk <andrew.chubatiuk@motional.com>
2022-12-27 16:46:20 +0200

    added example to ignorelist

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,5 +3,5 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
-  ignorePatterns: ['dist/**']
+  ignorePatterns: ['dist/**', 'example/**']
 }
